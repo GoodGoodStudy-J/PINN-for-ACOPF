a82c52afac5fbbdffa74e4f5245d289a37720b3e
GoodGoodStudy-J <bozhen.jiang@connect.polyu.hk>
2026-03-13 18:14:22 +0800

    Update Distribution_Display.py

--- Distribution_Display.py
+++ Distribution_Display.py
@@ -35,27 +35,17 @@ X_con_test = np.array(X_con_test_)/100
 
 
 
+from matplotlib import pyplot as plt
 fig = plt.figure(figsize=(5,2))
-plt.hist(X_con_train[:,0]*100,density=True,label="Training Set")
-plt.hist(X_con_test[:,0]*100,density=True,label="Testing Set")
+plt.hist(X_con_train.cpu().detach().numpy()[:,0]*100,density=True,label="Training Set")
+plt.hist(X_con_train_NL.cpu().detach().numpy()[:,0]*100,density=True,label="Trainin Set without Lable")
+plt.hist(X_con_test.cpu().detach().numpy()[:,0]*100,density=True,label="Testing Set")
 plt.xlabel("Load/MW")
 plt.ylabel("Probability Distribution \n Density")
 plt.legend()
 plt.grid()
 plt.tight_layout()
 plt.show()
-# fig.savefig("./distribution.svg",dpi=300)
+fig.savefig("./distribution.svg",dpi=300)
 
-# from matplotlib import pyplot as plt
-# fig = plt.figure(figsize=(5,2))
-# plt.hist(X_con_train.cpu().detach().numpy()[:,0]*100,density=True,label="Training Set")
-# plt.hist(X_con_train_NL.cpu().detach().numpy()[:,0]*100,density=True,label="Trainin Set without Lable")
-# plt.hist(X_con_test.cpu().detach().numpy()[:,0]*100,density=True,label="Testing Set")
-# plt.xlabel("Load/MW")
-# plt.ylabel("Probability Distribution \n Density")
-# plt.legend()
-# plt.grid()
-# plt.tight_layout()
-# plt.show()
-# fig.savefig("./distribution.svg",dpi=300)
 
